84a9474c29bd217d596801931239ef6c1231b95f
timka-byzov <tb@lamart.com>
2023-07-15 16:04:45 +0500

    it's alive

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -2,36 +2,87 @@ package storage
 
 import (
 	"context"
+	"sync"
+	// "sync"
 )
 
-// Result represents the Size function result
+const (
+	maxWorkersCount = 3
+)
+
 type Result struct {
-	// Total Size of File objects
-	Size int64
-	// Count is a count of File objects processed
+	Size  int64
 	Count int64
 }
 
 type DirSizer interface {
-	// Size calculate a size of given Dir, receive a ctx and the root Dir instance
-	// will return Result or error if happened
 	Size(ctx context.Context, d Dir) (Result, error)
 }
 
 // sizer implement the DirSizer interface
 type sizer struct {
-	// maxWorkersCount number of workers for asynchronous run
 	maxWorkersCount int
-
-	// TODO: add other fields as you wish
+	workerPool      chan struct{}
 }
 
 // NewSizer returns new DirSizer instance
 func NewSizer() DirSizer {
-	return &sizer{}
+	return &sizer{0, make(chan struct{}, maxWorkersCount)}
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	// TODO: implement this
-	return Result{}, nil
+	ch := make(chan File, 1000)
+
+	totalSize := int64(0)
+	count := int64(0)
+
+	go func() {
+		defer close(ch)
+		a.RecursiveSize(ctx, d, ch)
+	}()
+
+	for f := range ch {
+		select {
+		case <-ctx.Done():
+			return Result{}, ctx.Err()
+		default:
+			count++
+			size, err := f.Stat(ctx)
+			if err != nil {
+				return Result{}, err
+			}
+			totalSize += size
+		}
+	}
+
+	return Result{Size: totalSize, Count: count}, nil
+}
+
+func (a *sizer) RecursiveSize(ctx context.Context, d Dir, ch chan File) {
+	dirs, files, err := d.Ls(ctx)
+	if err != nil {
+		return
+	}
+
+	for _, f := range files {
+		select {
+		case ch <- f:
+		case <-ctx.Done():
+			return
+		}
+	}
+
+	var wg sync.WaitGroup
+	for _, d := range dirs {
+		wg.Add(1)
+		a.workerPool <- struct{}{} // Запрашиваем доступ к горутине из пула
+		go func(dir Dir) {
+			defer func() {
+				wg.Done()
+				<-a.workerPool // Освобождаем горутину в пуле
+			}()
+			a.RecursiveSize(ctx, dir, ch)
+		}(d)
+	}
+	wg.Wait()
 }
